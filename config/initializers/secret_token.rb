b9a7b10db577eaefde166073ae9a5e1eef7d5bb4
Geoff Hackett <ghackett@gmail.com>
2012-11-26 20:00:08 -0500

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZombieBits::Application.config.secret_token = '3ebfebb5a6d9be36dcfea92052216d6edbbf631763dfc35c611a57bf62b9a3ee240e7cf58a060a88c87092d19734744931d6c3bf96546b1fa94c47b4619154be'
